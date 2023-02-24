16c5ce278a65c618853030e5f08a2ff7d805154d
hawaiaanpizz4 <josuevla2011@gmail.com>
2023-02-24 11:24:27 -0500

    Agregando nuevos cambios

--- controller/verificacion.php
+++ controller/verificacion.php
@@ -5,6 +5,8 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 header('Content-Type: application/json');
 
+
+
 require_once("../models/Verificacion.php");
 $Verificacion = new Verificacion();
 
@@ -141,11 +143,59 @@ switch ($_SERVER['REQUEST_METHOD']) {
                     http_response_code(401);
                     echo json_encode(array("status" => "error", "message" => "Falta parametros"));
                 }
-                break;
+            break;
+            case 'send': 
+                var_dump("Hola");
+                die();
+                // Codigo
+                $DesdeLetra = "a";
+                $HastaLetra = "z";
+                $DesdeNumero = 1;
+                $HastaNumero = 10000;
+                $letraAleatoria = chr(rand(ord($DesdeLetra), ord($HastaLetra)));
+                $numeroAleatorio = rand($DesdeNumero, $HastaNumero);
+                $code = $letraAleatoria . $numeroAleatorio;
+                // end Codigo
+                $cellphone = $_GET["number"];
+
+                $cellphonemodify = substr($cellphone, 1);
+
+                $cellphonefinal = "593" . $cellphonemodify;
+
+                $url = "https://api2.massend.com/enviosms";
+
+                //Execute 
+                $curl = curl_init($url);
+
+                curl_setopt($curl, CURLOPT_URL, $url);
+                curl_setopt($curl, CURLOPT_POST, true);
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+
+                $headers = array(
+                    "Authorization: aWNlc3NhLWFwaUBtYXNzZW5kLmNvbQ==",
+                    "Content-Type: application/x-www-form-urlencoded",
+                );
+
+                curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+                $data = "username=icessa-api&mensajeid=32743&telefono=$cellphonefinal&tipo=1&datos=$code";
+
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+                $resp = curl_exec($curl);
+                $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+                if ($status == 201) {
+                    print_r(json_encode(array("status" => "OK", "message" => "Mensaje enviado, espere unos segundos, por favor...", "code"=> $code)));
+                } else {
+                    print_r(json_encode(array("status" => "error", "message" => "El mensaje no se pudo enviar")));
+                }
+            break;
+            
             default:
                 http_response_code(400);
                 echo $msgBadRequest;
                 return;
+            break;
         }
         break;
 
@@ -214,9 +264,3 @@ switch ($_SERVER['REQUEST_METHOD']) {
                 return;
         }
 }
-
-
-
-
-
-?>
